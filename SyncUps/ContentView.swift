2b3260f78e9e4da8bf3ef85585585ada0a51aeea
Tommy Han <tommyhonym@gmail.com>
2024-06-06 14:56:01 +0800

    Initial Commit

---
+++ SyncUps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SyncUps
+//
+//  Created by tommyhan on 6/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SyncUps/SyncUpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncUpsApp.swift
+//  SyncUps
+//
+//  Created by tommyhan on 6/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct SyncUpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
